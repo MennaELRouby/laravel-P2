46aa99d636423226a02511f6ac2f894eabfaa4fb
MennaELRouby <roubymenna@gmail.com>
2023-12-10 03:48:09 +0200

    9th session

---
+++ resources/views/layouts/article.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+@include('includes.head')
+
+
+<body> 
+
+@include('includes.header')
+		
+@include('includes.sliderarea')
+
+@yield('content')
+
+
+@include('includes.footer')
+@include('includes.footerjs')
+
+</body>
+</html>
